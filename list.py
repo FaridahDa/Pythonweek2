aefefd2270057b9a1ec659f3349cee363dbe34ed
Faridah <faridahdada@outlook.com>
2021-02-06 18:52:12 +0000

    added lists and tuples

---
+++ list.py
@@ -0,0 +1,22 @@
+# example of list
+fruits = ["Apple", "Mango", "Cherry"]
+print(fruits)
+
+#example of getting fruit from the list
+print(fruits[1])
+
+#example of getting the last fruit in the sequence
+print(fruits[-1])
+
+#length of the list
+print(len(fruits))
+
+#amending/adding to the list
+fruits.append("Melon")
+print(fruits)
+
+#adding to the list
+fruits2 = ["Strawberries", "Kiwis", "Grapes"]
+fruits.extend(fruits2)
+print(fruits)
+
---
+++ tuples.py
@@ -0,0 +1,14 @@
+#tuples exercise
+cities = ("London", "New York", "Berlin")
+print(cities)
+
+#check the type of data in the tuples
+print(type(cities))
+#question - why does it tell us the sequence type and not the text type
+
+#check the index
+print(cities.count("London"))
+
+#example of an if statement
+if "London" in cities :
+    print("Oxford street")
