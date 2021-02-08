df7c21f3f7dcc37516c567726bee9f079c90ae61
Faridah <faridahdada@outlook.com>
2021-02-08 19:28:24 +0000

    made changes to file

--- exercise9/types.py
+++ exercise9/types.py
@@ -1,8 +1,22 @@
 #space between first name and last name
-myFirstName = "Faridah"
-myLastName = "Dada"
-print(myFirstName + " " +myLastName)
+First = "Faridah"
+Last = "Dada"
+print(First + " " + Last)
 
-name = myFirstName , myLastName
+#list constructor vs just typing a list
+#name = [First, Last]
+#name = list ((First ,Last))
+name = ["Faridah", "Dada"]
 print(name)
 
+#converting list to dictonary
+dict = ["First", " Last" ]
+
+#namedict = dict(zip(name,dict))
+#namedict = dict.fromkeys(name, dict)
+#print(namedict)
+
+#converting list to dictonary
+keys = {'a', 'e', 'i'}
+vowels = dict.fromkeys(keys)
+print(vowels)
--- list.py
+++ list.py
@@ -20,3 +20,11 @@ fruits2 = ["Strawberries", "Kiwis", "Grapes"]
 fruits.extend(fruits2)
 print(fruits)
 
+#example of a set, why hasn't our set changed order & how do we create a loop with a set?
+veg = {"sprouts", "onions", "lettuce", "carrots" }
+print(veg)
+print(veg)
+print(veg)
+x = 1
+for x in veg:
+    print(x)
